18d4c4ee72c15489180d17a0bb589a2fcfc03580
TanishqMeshram <131801414+TanishqMeshram@users.noreply.github.com>
2024-09-26 05:53:07 +0530

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://examprep-0o81.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
